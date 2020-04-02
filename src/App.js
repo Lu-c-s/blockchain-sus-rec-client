b9a9d9f30c7fb5f998551765251c34cb18342f5b
Lu-c-s <Lucas3645@gmail.com>
2020-04-02 20:28:01 -0300

    Added react-router and Provider and patient page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import LoginPage from './pages/LoginPage'
-import './App.css';
+import React, { useState } from "react";
+import LoginPage from "./pages/LoginPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
+const USER_TYPE = {
+  PATIENT: true,
+  PROVIDER: false
+};
 
 function App() {
+  const [user, setUser] = useState(USER_TYPE.PATIENT);
+
   return (
     <div className="App">
-      <LoginPage />
+      <LoginPage setUserType={user => setUser(user)} currentUser={user} />
     </div>
   );
 }

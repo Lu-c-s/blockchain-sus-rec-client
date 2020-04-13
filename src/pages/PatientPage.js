da301344b1ecfcb36f3ca0b8135a1cc7c540950c
Lu-c-s <Lucas3645@gmail.com>
2020-04-13 19:29:51 -0300

    Added changes to lateral menu

--- src/pages/PatientPage.js
+++ src/pages/PatientPage.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
-export default function componentName() {
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const PatientPage = (props) => {
+  const [Collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+
   return (
-    <>
-      <p>Patient page</p>
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={Collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1">
+            <PieChartOutlined />
+            <span>Dados pessoais</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <DesktopOutlined />
+            <span>Registros médicos</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
-}
+};
+
+export default PatientPage;
--- src/pages/ProviderPage.js
+++ src/pages/ProviderPage.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
-export default function componentName() {
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const ProviderPage = (props) => {
+  const [Collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+
   return (
-    <>
-      <p>Provider page</p>
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={Collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1">
+            <PieChartOutlined />
+            <span>Dados pessoais</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <DesktopOutlined />
+            <span>Registros</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Patient Data
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
-}
+};
+
+export default ProviderPage;
